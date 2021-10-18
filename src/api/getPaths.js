9299ce5aef00636f39e84d7da76c861251d59b4b
Peter Warrington <sothisispeter@gmail.com>
2021-10-18 15:15:52 +0100

    Fix point options not being passed to client

--- src/api/getPaths.js
+++ src/api/getPaths.js
@@ -5,25 +5,23 @@
  * @param {*} res 
  */
 module.exports.getPaths = function (shared, req, res) {
-    canvasState = null;
-
     // Create path 1
-    var startingPathPart1 = new shared.PathPart(new shared.MapPoint(50, 50, canvasState, {pointText: "📍\t\tPath 1"}));
-    startingPathPart1.connectingTo(new shared.MapPoint(60, 55, canvasState))
-    .connectingTo(new shared.MapPoint(100, 70, canvasState))
-    .connectingTo(new shared.MapPoint(110, 100, canvasState));
+    var startingPathPart1 = new shared.PathPart(new shared.MapPoint(50, 50, {pointText: "📍\t\tPath 1"}));
+    startingPathPart1.connectingTo(new shared.MapPoint(60, 55))
+    .connectingTo(new shared.MapPoint(100, 70))
+    .connectingTo(new shared.MapPoint(110, 100));
 
-    var path1 = new shared.Path(startingPathPart1, canvasState, "Path1");
+    var path1 = new shared.Path(startingPathPart1, "Path1");
 
     // Create path 2
-    var startingPathPart2 = new shared.PathPart(new shared.MapPoint(30, 200, canvasState, {pointText: "📍\t\tPath 2"}));
-    startingPathPart2.connectingTo(new shared.MapPoint(400, 170, canvasState))
-    .connectingTo(new shared.MapPoint(300, 250, canvasState))
-    .connectingTo(new shared.MapPoint(270, 20, canvasState))
-    .connectingTo(new shared.MapPoint(120, 100, canvasState))
-    .connectingTo(new shared.MapPoint(160, 160, canvasState));
+    var startingPathPart2 = new shared.PathPart(new shared.MapPoint(30, 200, {pointText: "📍\t\tPath 2"}));
+    startingPathPart2.connectingTo(new shared.MapPoint(400, 170))
+    .connectingTo(new shared.MapPoint(300, 250))
+    .connectingTo(new shared.MapPoint(270, 20))
+    .connectingTo(new shared.MapPoint(120, 100))
+    .connectingTo(new shared.MapPoint(160, 160));
 
-    var path2 = new shared.Path(startingPathPart2, canvasState, "Path2");
+    var path2 = new shared.Path(startingPathPart2, "Path2");
 
     var paths = [path1, path2];
 
